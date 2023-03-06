42351b47bfcf4bdf118a7ab4f297e1e4eaa34c42
Kirill Lyustochkin <KLyustochkin@sitronics.com>
2023-03-06 14:37:24 +0300

    HW1 increasing the performance of the concurrentMultiply method

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 /**
  * gkislin
@@ -13,25 +14,66 @@ public class MatrixUtil {
 
     // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
-        Callable<int[][]> task = () -> getMultipliedMatrix(matrixA, matrixB);
-        return executor.submit(task).get();
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+        Callable<int[][]> task;
+        Future<int[][]> future = null;
+
+        for (int i = 0; i < matrixSize; i++) {
+            final int[] columnB = new int[matrixSize];
+            for (int j = 0; j < matrixSize; j++) {
+                columnB[j] = matrixB[j][i];
+            }
+
+            int finalI = i;
+            task = () -> {
+                for (int k = 0; k < matrixSize; k++) {
+                    int[] rowA = matrixA[k];
+                    int sum = 0;
+                    for (int m = 0; m < matrixSize; m++) {
+                        sum += rowA[m] * columnB[m];
+                    }
+                    matrixC[k][finalI] = sum;
+                }
+                return matrixC;
+            };
+            future = executor.submit(task);
+        }
+        return future.get();
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
+        //  return getMultipliedMatrix(matrixA, matrixB);
 
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        return getMultipliedMatrix(matrixA, matrixB);
+        for (int i = 0; i < matrixSize; i++) {
+            final int[] columnB = new int[matrixSize];
+            for (int j = 0; j < matrixSize; j++) {
+                columnB[j] = matrixB[j][i];
+            }
+
+            for (int k = 0; k < matrixSize; k++) {
+                int[] rowA = matrixA[k];
+                int sum = 0;
+                for (int m = 0; m < matrixSize; m++) {
+                    sum += rowA[m] * columnB[m];
+                }
+                matrixC[k][i] = sum;
+            }
+        }
+        return matrixC;
     }
 
-    private static int[][] getMultipliedMatrix(int[][] matrixA, int[][] matrixB) {
+/*    private static int[][] getMultipliedMatrix(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
             for (int i = 0; i < matrixSize; i++) {
                 final int[] columnB = new int[matrixSize];
                 for (int j = 0; j < matrixSize; j++) {
-                    System.arraycopy(matrixB[j], 0, columnB, 0, matrixSize);
                     columnB[j] = matrixB[j][i];
                 }
 
@@ -45,7 +87,7 @@ public class MatrixUtil {
                 }
             }
         return matrixC;
-    }
+    }*/
 
 
     public static int[][] create(int size) {
