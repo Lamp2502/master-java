956c5b28bf8b3b22694fd6beab53fa1a5cb1e4c9
Kirill Lyustochkin <KLyustochkin@sitronics.com>
2023-03-06 11:58:11 +0300

    HW1 1. optimize singleThreadMultiply -> getMultipliedMatrix in MatrixUtil 2. implement concurrentMultiply

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,6 +1,7 @@
 package ru.javaops.masterjava.matrix;
 
 import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 
@@ -14,26 +15,42 @@ public class MatrixUtil {
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
+        int[] columnB = new int[matrixSize];
 
-        return matrixC;
+        Callable<int[][]> task = () -> getMultipliedMatrix(matrixA, matrixB, matrixSize, matrixC, columnB);
+
+        return executor.submit(task).get();
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
+        int[] columnB = new int[matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
+        return getMultipliedMatrix(matrixA, matrixB, matrixSize, matrixC, columnB);
+    }
+
+    private static int[][] getMultipliedMatrix(int[][] matrixA, int[][] matrixB, int matrixSize, int[][] matrixC, int[] columnB) {
+        try {
+            for (int i = 0; i < matrixSize; i++) {
+                for (int j = 0; j < matrixSize; j++) {
+                    System.arraycopy(matrixB[j], 0, columnB, 0, matrixSize);
                 }
-                matrixC[i][j] = sum;
+
+                for (int k = 0; k < matrixSize; k++) {
+                    int[] rowA = matrixA[k];
+                    int sum = 0;
+                    for (int m = 0; m < matrixSize; m++) {
+                        sum += rowA[m] * columnB[m];
+                    }
+                    matrixC[k][i] = sum;
+                }
             }
-        }
+        } catch (IndexOutOfBoundsException ignored) { }
         return matrixC;
     }
+
 
     public static int[][] create(int size) {
         int[][] matrix = new int[size][size];
